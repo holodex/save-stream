1c9f7e126d47d22772a661a45efb3a4c63f04b9e
Michael Williams <michael.williams@enspiral.com>
2015-08-29 01:06:50 +1200

    better way of streaming

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var through = require('through2');
 module.exports = function () {
     var buffer = [];
     var finished = false;
+    var piping = false;
+    var dests = 0;
     var passThrough = through.obj(function (obj, enc, cb) {
         buffer.push(obj);
-        this.emit('data', obj);
-        cb(null);
+        if (piping || dests > 0) {
+            cb(null, obj);
+        } else {
+            cb(null)
+        }
     }, function (cb) {
         finished = true;
         cb();
@@ -14,12 +19,16 @@ module.exports = function () {
 
     passThrough._buffer = buffer;
 
+    passThrough.on('pipe', function () { dests++ })
+    passThrough.on('unpipe', function () { dests-- })
+
     passThrough.load = function () {
         var loaded = through.obj();
         buffer.map(loaded.write, loaded);
         if (finished) {
             loaded.end();
         } else {
+            piping = true
             passThrough.pipe(loaded);
         }
         return loaded;
